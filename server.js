551291dead25420086e6513f842976ba7640e1a6
Glitch (garrulous-willow) <none>
2018-03-07 22:41:50 +0000

    😜🌛 Checkpoint public/style.css:718707/18 server.js:718707/244 views/index.html:718707/37

--- server.js
+++ server.js
@@ -1,37 +1,28 @@
-// server.js
-// where your node app starts
-
-// init project
 var express = require('express');
+var multer = require('multer');
+var cors = require('cors');
+
+var upload = multer({ dest: 'uploads/' });
+
 var app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 // could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
+app.post("/file", upload.single('file'), function (req, res) {
+  console.log(req);
+  res.redirect("/");
 });
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
